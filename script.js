06d203f5c91da63ec38f027e0904df00c0ad3278
Olaisheretolearn <63797098+Olaisheretolearn@users.noreply.github.com>
2022-01-29 06:23:09 -0600

    Now shows Transaction items

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
 const money_minus= document.getElementById('money-minus');
-const balance = document.getElementById('balance');
-const balance = document.getElementById('balance');
-const balance = document.getElementById('balance');
-const balance = document.getElementById('balance');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const dummyTransactions = [
+    { id:1, text: 'Flower', amount: -20},
+    { id:1, text: 'Salary', amount: 300},
+    { id:1, text: 'Book', amount: -10},
+    { id:1, text: 'Camera', amount: 150},
+];
+
+let transactions = dummyTransactions;
+
+// add transactions to dom list
+function addTransactionDOM(transaction){
+    // get sizing
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    const item = document.createElement('li');
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = ` ${transaction.text} <span> ${sign} ${Math.abs(transaction.amount)} 
+     </span> <button class="delete-btn">x</button>`;
+
+
+list.appendChild(item);
+
+}
+
+function init() {
+    list.innerHTML ='';
+
+    transactions.forEach(addTransactionDOM)
+        
+   }
+
+init();
